2efd54b45c63599f110fe1e1dbe5baa097ce6c09
mollameehedi <mollameehedi@gmail.com>
2021-09-19 19:39:31 +0600

    create redux and public globally from modues 18 of video 1,2

---
+++ src/redux/reducer.js
@@ -0,0 +1,11 @@
+import COMMENTS from "../data/comments"
+import DISHES from "../data/dishes"
+
+const initialState = {
+    comments:COMMENTS,
+    dishes : DISHES
+}
+
+export const Reducer = (state = initialState, action )=> {
+    return state
+}
